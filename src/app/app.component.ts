d40d985e9a46c4549ac98262df078099af85f34f
Armand Deshais <armand.deshais@gmail.com>
2021-03-31 14:11:22 +0200

    auth services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appcrm';
+  constructor(public authService: AuthService, private router: Router) {
+
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/').then(r => {
+      console.error(r);
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,58 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {User} from '../models';
+import {catchError, map} from 'rxjs/operators';
+
+
+const apiUrl = 'http://localhost:8000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private currentClientSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentClientSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentClientSubject.asObservable();
+  }
+
+  public get CurrentClientValue(): User {
+    return this.currentClientSubject.value;
+
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      return of(result as T);
+    };
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<User>(apiUrl + 'api/login', {username, password})
+      .pipe(map(
+        userData => {
+          localStorage.setItem('currentUser', JSON.stringify(userData));
+          return userData;
+        }
+        ),
+        catchError(this.handleError<User>('errorLogin')));
+  }
+
+  isUserLoggedIn() {
+    const user = localStorage.getItem('currentUser');
+    console.log(!(user === null));
+    return !(user === null);
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+  }
+
+  register(identifiant: User) {
+    return this.http.post<User>(apiUrl + '/api/signup', identifiant);
+  }
+}
